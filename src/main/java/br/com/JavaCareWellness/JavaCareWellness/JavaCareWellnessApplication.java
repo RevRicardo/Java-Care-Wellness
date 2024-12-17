045b65016e3d9dcbbe2b836e8cc60af04f22ebfc
Ricardo Lacerda <revricardolacerda@gmail.com>
2024-12-17 14:51:33 -0300

    Inicia projeto

---
+++ src/main/java/br/com/JavaCareWellness/JavaCareWellness/JavaCareWellnessApplication.java
@@ -0,0 +1,13 @@
+package br.com.JavaCareWellness.JavaCareWellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCareWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCareWellnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/JavaCareWellness/JavaCareWellness/JavaCareWellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.JavaCareWellness.JavaCareWellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCareWellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
