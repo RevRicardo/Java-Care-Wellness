8eaf8440d05e42b5bb9e256a7fa0a85890fc5b59
Ricardo Lacerda <revricardolacerda@gmail.com>
2024-12-17 16:28:53 -0300

    Implementa fluxo para service

---
+++ src/main/java/br/com/JavaCareWellness/JavaCareWellness/beneficiario/appication/service/BeneficiarioServide.java
@@ -0,0 +1,9 @@
+package br.com.JavaCareWellness.JavaCareWellness.beneficiario.appication.service;
+
+import br.com.JavaCareWellness.JavaCareWellness.beneficiario.appication.api.BeneficiarioRequest;
+import br.com.JavaCareWellness.JavaCareWellness.beneficiario.appication.api.BeneficiarioResponse;
+
+public interface BeneficiarioServide {
+	BeneficiarioResponse criaBeneficiario(BeneficiarioRequest beneficiarioRequest);
+
+}
