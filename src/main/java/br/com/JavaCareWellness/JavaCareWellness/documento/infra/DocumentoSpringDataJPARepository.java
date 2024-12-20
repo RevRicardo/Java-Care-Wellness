831c9477dc65b10015809468252d4eaafc3e68a9
Ricardo Lacerda <revricardolacerda@gmail.com>
2024-12-20 07:13:47 -0300

    Implementa repository infra documento busca documentos por id beneficiario

--- src/main/java/br/com/JavaCareWellness/JavaCareWellness/documento/infra/DocumentoSpringDataJPARepository.java
+++ src/main/java/br/com/JavaCareWellness/JavaCareWellness/documento/infra/DocumentoSpringDataJPARepository.java
@@ -1,5 +1,6 @@
 package br.com.JavaCareWellness.JavaCareWellness.documento.infra;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.JavaCareWellness.JavaCareWellness.documento.domain.Documento;
 
 public interface DocumentoSpringDataJPARepository extends JpaRepository<Documento, UUID> {
-	
+	List<Documento> findByIdBeneficiarioDoc(UUID idBeneficiarioDoc);
 }
