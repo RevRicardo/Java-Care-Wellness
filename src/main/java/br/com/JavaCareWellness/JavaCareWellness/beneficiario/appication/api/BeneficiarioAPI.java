4d180efc11b81c78de2ad626feb51e0533506ddd
Ricardo Lacerda <revricardolacerda@gmail.com>
2024-12-21 08:13:04 -0300

    Implementa swagger

--- src/main/java/br/com/JavaCareWellness/JavaCareWellness/beneficiario/appication/api/BeneficiarioAPI.java
+++ src/main/java/br/com/JavaCareWellness/JavaCareWellness/beneficiario/appication/api/BeneficiarioAPI.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("/beneficiario/v1.0")
 public interface BeneficiarioAPI {
